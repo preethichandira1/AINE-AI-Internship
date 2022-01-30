3424ded6e7a6141f63d5d5704541acf936c49dec
preethichandira1 <98679840+preethichandira1@users.noreply.github.com>
2022-01-30 15:59:42 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import *
+
+import os
+from werkzeug.utils import secure_filename
+from predict import predict_covid
+application = Flask(__name__)
+
+@application.route('/')
+def upload():
+    return render_template("index.html")
+
+@application.route('/success', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'input_data', secure_filename(f.filename))
+        f.save(file_path)
+
+
+
+
+
+
+        outcome = predict_covid(file_path)
+
+        return str(outcome)
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    application.run(debug=True)
